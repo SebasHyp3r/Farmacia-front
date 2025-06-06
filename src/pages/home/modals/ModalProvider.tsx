a99759183b5a4161f600d0cf5457947b88895f6e
Flavia <flaviasicha161@gmail.com>
2025-06-06 14:37:11 -0500

    a

--- src/pages/home/modals/ModalProvider.tsx
+++ src/pages/home/modals/ModalProvider.tsx
@@ -1,32 +1,32 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+// import { createContext, ReactNode, useContext, useState } from "react";
 
-const ChatContext = createContext(false)
-const UpdateChatContext = createContext(() => {})
+// const ChatContext = createContext(false)
+// const UpdateChatContext = createContext(() => {})
 
-export function useChatContext () {
-  return useContext(ChatContext)
-}
+// export function useChatContext () {
+//   return useContext(ChatContext)
+// }
 
-export function useUpdateChatContext () {
-  return useContext(UpdateChatContext)
-}
+// export function useUpdateChatContext () {
+//   return useContext(UpdateChatContext)
+// }
 
 
 
-export function HomeModalProvider ({children} : {children: ReactNode}) {
+// export function HomeModalProvider ({children} : {children: ReactNode}) {
 
-  const [chatModal, setChatModal] = useState(false)
+//   // const [chatModal, setChatModal] = useState(false)
 
-  const a = () => {
-    console.log("kajsdks")
-  }
+//   const a = () => {
+//     console.log("kajsdks")
+//   }
 
-  return (
-    <ChatContext.Provider value={chatModal}>
-      <UpdateChatContext.Provider value={a}>
-        {children}
-      </UpdateChatContext.Provider>
-    </ChatContext.Provider>
-  )
+//   return (
+//     <ChatContext.Provider value={chatModal}>
+//       <UpdateChatContext.Provider value={a}>
+//         {children}
+//       </UpdateChatContext.Provider>
+//     </ChatContext.Provider>
+//   )
 
-}
+// }
